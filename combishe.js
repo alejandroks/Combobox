4fe22918797fe0755687f38eca54d8c58dd9e52e
Alex <alejandroks02@gmail.com>
2016-10-18 14:39:18 +0200

    combobox Part 2

--- combishe.js
+++ combishe.js
@@ -10,21 +10,28 @@ function MyCombobox(object){
     var buttonSkroll = document.createElement('div');
     buttonSkroll.className="Skroling";
     buttonSkroll.innerHTML="X"
+
+    var divOverFlow=document.createElement('div');
+    divOverFlow.className='divOwer';
     buttonSkroll.onclick=function onClikButton(object1)
     {
         var listUL=document.createElement('ul');
         listUL.className="listul";
+
         var list=[];
         for(var i=0;i<object.dataSource.length;i++){
             list[i] = document.createElement('li');
             list[i].innerHTML=object.dataSource[i];
             listUL.appendChild(list[i]);
         }
-        object.current.appendChild(listUL);
+        divOverFlow.appendChild(listUL)
+        object.current.appendChild(divOverFlow);
+
     }
     wrapper.appendChild(listString);
     wrapper.appendChild(buttonSkroll);
-    object.current.appendChild(wrapper)
+    object.current.appendChild(wrapper);
+
 
 
 
